9fc14d297be003096216e9d9b87adfdffa0a3567
NaveenHashira <naveenmathaiyan12@gmail.com>
2025-02-02 10:12:05 +0530

    only positive

--- kumar.py
+++ kumar.py
@@ -1,6 +1,7 @@
-z = int(input("Enter a number"))
-if z > 0:
-    print("positive")
-else:
-    print("negative")
-    print("sumaar")
+from logging import exception
+
+try:
+    z = int(input("Enter a number: "))
+    if z > 0:
+        print("positive")
+
